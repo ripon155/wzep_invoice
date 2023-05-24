d5f5ed7524a4a343eb15d9c56c26e61266494391
ripon155 <mohiuddin.saifullah@gmail.com>
2023-05-24 12:58:34 +0600

    some changes

--- src/app/Component/customer-details/customer-details.component.ts
+++ src/app/Component/customer-details/customer-details.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-customer-details',
   templateUrl: './customer-details.component.html',
-  styleUrls: ['./customer-details.component.css']
+  styleUrls: ['./customer-details.component.css'],
 })
 export class CustomerDetailsComponent {
+  name: string = 'Ripon';
+  contact: string = '01724446252';
+  address: string = 'Tangil, Bangladesh';
+  postCode: string = '1900';
+  place: string = 'Santosh';
+  vatNumber: string = '1234';
+  inputState: string = 'Bangladesh';
 
+  onSubmit(form: NgForm) {
+    console.log(form.form.value);
+    console.log(this.name);
+  }
 }
