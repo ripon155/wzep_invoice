60cb12865934c249b4103eae4faebedb5ad9fd39
ripon155 <mohiuddin.saifullah@gmail.com>
2023-06-15 16:34:41 +0600

    get countries

--- src/app/Component/customer-details/customer-details.component.ts
+++ src/app/Component/customer-details/customer-details.component.ts
@@ -1,22 +1,50 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+
+interface Country {
+  id: number;
+  name: string;
+  iso: string;
+  iso3: string;
+  numcode: number;
+  phonecode?: number; // Optional property, add if necessary
+}
 
 @Component({
   selector: 'app-customer-details',
   templateUrl: './customer-details.component.html',
   styleUrls: ['./customer-details.component.css'],
 })
-export class CustomerDetailsComponent {
+export class CustomerDetailsComponent implements OnInit {
   name: string = 'Ripon';
   contact: string = '01724446252';
   address: string = 'Tangil, Bangladesh';
   postCode: string = '1900';
   place: string = 'Santosh';
   vatNumber: string = '1234';
-  inputState: string = 'Bangladesh';
+  state: string = 'BANGLADESH';
+
+  countries: Country[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getCountry();
+  }
 
   onSubmit(form: NgForm) {
     console.log(form.form.value);
     console.log(this.name);
+  }
+
+  public getCountry() {
+    this.http
+      .get<Country[]>('https://symfony.wezp/invoiceapi/invoiceapi/id')
+      .subscribe((data: any) => {
+        if (data) {
+          this.countries = JSON.parse(data);
+        }
+      });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,7 +18,7 @@ import { CustomerDetailsComponent } from './Component/customer-details/customer-
     ProductDetailsComponent,
     CustomerDetailsComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
